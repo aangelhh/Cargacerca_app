6b6b44f6a3b553620ca5ec64476b24102136ae97
aangelhh <aangelhh@gmail.com>
2026-09-08 00:35:05 +0200

    refactor(shared): add coroutines for shared loading

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -30,6 +30,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation("io.ktor:ktor-client-core:3.5.2")
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
             implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
         }
 
