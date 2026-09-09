b7aec6a7cb8baab90db5ac78245d5d55f9607cbb
aangelhh <aangelhh@gmail.com>
2026-09-09 20:05:39 +0200

    chore(android): bump version to 0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "es.cargacerca.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.8.0"
+        versionCode = 9
+        versionName = "0.9.0"
     }
 
     buildTypes {
